1113adc2f0167ace06c9d52ff536a350f6b3a73b
Graham Schilling <schill1025@gmail.com>
2014-07-02 16:50:11 -0400

    profile pictures from twitter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
       auth = request.env["omniauth.auth"]
       user =  User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
       session[:user_id] = user.id
+      info = auth["info"]
+      user.picture = info["image"]
       redirect_to members_path, :notice => "Signed in!"
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140701134849) do
+ActiveRecord::Schema.define(version: 20140702203514) do
 
   create_table "bets", force: true do |t|
     t.string   "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20140701134849) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
+    t.string   "picture"
   end
 
 end
