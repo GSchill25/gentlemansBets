73c6d475bb0341073209a9b567bbbde9af3e2421
Graham Schilling <gschilli@andrew.cmu.edu>
2014-09-10 17:27:59 -0400

    remove old chart js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require chartkick
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
+//= require chart.js
 //= require jquery_nested_form
 //= require foundation
 //= require_tree .
